b6128b47f4d9b2af79db4246358b93195ddbdf55
ZAINAB AL KHBORI <zainab@mgc.loc>
2020-11-02 16:33:53 +0400

    save

--- TurnJs/public/script.js
+++ TurnJs/public/script.js
@@ -8,47 +8,82 @@ async function book(book, scale) {
 	var loadingTask = pdfjsLib.getDocument(url);
 	var pdf = await loadingTask.promise;
 
-	for (var i = 0; i < pdf.numPages; i++) {
-		var bookPage = $("<div class='page p" + (i + 1) + "'><canvas></canvas></div>");
-		var canvas = $("canvas", bookPage);
+	var numPages = pdf.numPages % 2 === 1 ? pdf.numPages + 1 : pdf.numPages;
+	for (var i = 0; i < numPages; i++) {
+		var bookPage = $("<div class='p" + (i + 1) + "'><canvas></canvas></div>");
 
-		if (i < 2 || i > pdf.numPages - 3) {
+		if (i < 2 || i > (numPages - 3)) {
 			bookPage.addClass("hard");
 		}
 
-
-
+		bookPage.append("<div class='gradient'></div>");
 		$(book).append(bookPage);
 	}
 
-	// for (var i = 1; i <= pdf.numPages; i++) {
-	// 	var page = await pdf.getPage(i);
-	// 	var pageDiv = $("<div class='page'></div>");
+	var page = await pdf.getPage(1);
+	var viewport = page.getViewport({ scale: scale });
+
+	$(book).turn({
+		width: viewport.width * 2,
+		height: viewport.height,
+		autoCenter: true
+	});
+
+	var canvas = $("canvas", ".p1");
+	var context = canvas[0].getContext("2d");
+	canvas.attr("width", viewport.width);
+	canvas.attr("height", viewport.height);
+
+	var renderContext = {
+		canvasContext: context,
+		viewport: viewport
+	};
+
+	page.render(renderContext);
+
+	// for (var i = 2; i <= pdf.numPages; i++) {
+	// 	page = await pdf.getPage(i);
+	// 	viewport = page.getViewport({ scale: scale });
 	//
-	// 	var canvas = $("<canvas></canvas>");
-	// 	var context = canvas[0].getContext("2d");
-	// 	var viewport = page.getViewport({ scale: scale });
+	// 	canvas = $("canvas", ".p" + i);
+	// 	context = canvas[0].getContext("2d");
 	//
 	// 	canvas.attr("width", viewport.width);
 	// 	canvas.attr("height", viewport.height);
-	// 	$(book).append(pageDiv);
-	// 	$(pageDiv).append(canvas);
 	//
-	// 	var renderContext = {
+	// 	renderContext = {
 	// 		canvasContext: context,
 	// 		viewport: viewport
 	// 	};
 	//
-	// 	page.render(renderContext);
-	//
-	// 	if (i === 3) {
-	// 		$(".flipbook").turn({
-	// 			width: viewport.width * 2,
-	// 			height: viewport.height,
-	// 			autoCenter: true
-	// 		});
-	// 	}
+	// 	// page.render(renderContext);
 	// }
+
+	var rendered = Array(pdf.numPages).fill(false);
+	rendered[0] = true;
+
+	$(book).on("turning", async function(e, pageNum, view) {
+		var range = $(book).turn("range");
+		for (var i = range[0]; i <= range[1]; i++) {
+			if ($(book).turn("hasPage", i) && !rendered[i - 1]) {
+				var page = await pdf.getPage(i);
+				var viewport = page.getViewport({ scale: scale });
+
+				var canvas = $("canvas", ".p" + i);
+				var context = canvas[0].getContext("2d");
+
+				canvas.attr("width", viewport.width);
+				canvas.attr("height", viewport.height);
+
+				var renderContext = {
+					canvasContext: context,
+					viewport: viewport
+				};
+
+				page.render(renderContext);
+			}
+		}
+	});
 
 	// var textContent = await page.getTextContent();
 	// var div = $("<div class='textLayer'></div>");
@@ -68,50 +103,3 @@ async function book(book, scale) {
 $(".book").each(async function() {
 	await book(this, 1);
 });
-
-// $(document).ready(function() {
-// 	var book = "Annual%20Report%20Arabic%202015.pdf";
-// 	fetch("/converted/" + book)
-// 		.then(function(res) { return res.json(); })
-// 		.then(function(res) {
-// 			var pages = res.data;
-// 			var bookHeight = $(".flipbook").outerHeight(true);
-// 			var width = res.size.width;
-// 			var height = res.size.height;
-//
-// 			var factor = bookHeight / height;
-// 			var bookWidth = width * factor;
-//
-// 			// console.log("bookHeight = ", bookHeight);
-// 			// console.log("height = ", height);
-// 			// console.log("factor = ", factor);
-// 			console.log("width = ", width);
-// 			console.log("bookWidth = ", bookWidth);
-//
-// 			for (var page of pages) {
-// 				var iframe = $("<div class='page'><iframe></iframe></div>");
-//
-// 				var binary = atob(page.split(",")[1]);
-// 				var bytes = new Uint8Array(binary.length);
-//
-// 				for (var i = 0; i < binary.length; i++) {
-// 					bytes[i] = binary.charCodeAt(i);
-// 				}
-//
-// 				var blob = new Blob([bytes], { type: "application/pdf" });
-// 				var uri = "/web/viewer.html?file=" + URL.createObjectURL(blob);
-//
-// 				$("iframe", iframe).attr("width", bookWidth);
-// 				// $("iframe", iframe).attr("height", bookHeight);
-// 				$("iframe", iframe).attr("src", uri);
-//
-// 				$(".flipbook").append(iframe);
-// 			}
-//
-// 			$(".flipbook").turn({
-// 				width: bookWidth * 2,
-// 				height: bookHeight,
-// 				autoCenter: true
-// 			});
-// 		});
-// });
