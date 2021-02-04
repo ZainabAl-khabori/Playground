1a3f82ed11f798612eceeb4144023aa0a40693cd
ZainabAl-khaboi <hime.chi@hotmail.com>
2021-02-04 15:49:45 +0400

    save

--- Animated Pie Chart/script.js
+++ Animated Pie Chart/script.js
@@ -25,6 +25,40 @@ function pieChart(container, data, minRadius, barWidth, pathFill, indicatorFill)
 		.attr("fill", pathFill)
 		.attr("d", path);
 
+	var bars = target.selectAll(".data").data(data).enter().append("path")
+		.attr("class", "data")
+		.attr("fill", function(d, i) { return d.fill });
+
+	var innerDots = target.selectAll(".dot-in").data(data).enter().append("circle")
+		.attr("class", "dot-in")
+		.attr("fill", indicatorFill)
+		.attr("r", 0)
+		.attr("cx", -8)
+		.attr("cy", function(d) { return minRadius + (barWidth * d.index) + (barWidth / 2); });
+
+	var lines = target.selectAll(".line").data(data).enter().append("line")
+		.attr("class", "line")
+		.attr("stroke", indicatorFill)
+		.attr("x1", -8)
+		.attr("x2", -8)
+		.attr("y1", function(d) { return minRadius + (barWidth * d.index) + (barWidth / 2); })
+		.attr("y2", function(d) { return minRadius + (barWidth * d.index) + (barWidth / 2); });
+
+	var outerDots = target.selectAll(".dot-out").data(data).enter().append("circle")
+		.attr("class", "dot-out")
+		.attr("fill", indicatorFill)
+		.attr("r", 0)
+		.attr("cx", 150)
+		.attr("cy", function(d) { return minRadius + (barWidth * d.index) + (barWidth / 2); });
+
+	var labels = target.selectAll(".labels").data(data).enter().append("text")
+		.attr("class", "label")
+		.attr("fill", indicatorFill)
+		.attr("font-size", 0)
+		.attr("x", 160)
+		.attr("y", function(d) { return minRadius - 3 + (barWidth * (d.index + 1)); })
+		.text(function(d, i) { return d.label + " (" + d.value + "%)" });
+
 	function barsTween(b) {
 		var i = d3.interpolate({ value: 0 }, b);
 		return function(t) {
@@ -33,61 +67,45 @@ function pieChart(container, data, minRadius, barWidth, pathFill, indicatorFill)
 	}
 
 	setTimeout(function() {
-		target.selectAll(".data").data(data).enter().append("path")
-			.attr("class", "data")
-			.attr("fill", function(d, i) { return d.fill })
-			.transition()
+		bars.transition()
 			.ease(d3.easeElastic)
 			.duration(1000)
 			.delay(function(d, i) { return i * 100; })
 			.attrTween("d", barsTween);
 
-		target.selectAll(".dot-in").data(data).enter().append("circle")
-			.attr("class", "dot-in")
-			.attr("fill", indicatorFill)
-			.attr("r", 0)
-			.attr("cx", -8)
-			.attr("cy", function(d) { return minRadius + (barWidth * d.index) + (barWidth / 2); })
-			.transition()
+		innerDots.transition()
 			.ease(d3.easeElastic)
 			.duration(1000)
 			.delay(function(d, i) { return i * 100; })
 			.attr("r", 3);
 
-		target.selectAll(".dash").data(data).enter().append("line")
-			.attr("class", "dash")
-			.attr("stroke", indicatorFill)
-			.attr("x1", -8)
-			.attr("x2", -8)
-			.attr("y1", function(d) { return minRadius + (barWidth * d.index) + (barWidth / 2); })
-			.attr("y2", function(d) { return minRadius + (barWidth * d.index) + (barWidth / 2); })
-			.transition()
+		lines.transition()
 			.ease(d3.easeLinear)
 			.duration(150)
 			.delay(function(d, i) { return i * 100; })
 			.attr("x2", 150)
 
-		target.selectAll(".dot-out").data(data).enter().append("circle")
-			.attr("class", "dot-out")
-			.attr("fill", indicatorFill)
-			.attr("r", 0)
-			.attr("cx", 150)
-			.attr("cy", function(d) { return minRadius + (barWidth * d.index) + (barWidth / 2); })
-			.transition()
+		outerDots.transition()
 			.ease(d3.easeElastic)
 			.duration(1000)
 			.delay(function(d, i) { return (i * 100) + 150; })
 			.attr("r", 3)
 			.attr("fill", function(d, i) { return d.fill });
+
+		labels.transition()
+			.ease(d3.easeElastic)
+			.duration(1000)
+			.delay(function(d, i) { return (i * 100) + 150; })
+			.attr("font-size", 12);
 	}, 400);
 }
 
 var data = [
-	{ "index": 0, "value": 100, "fill": "#00833e" },
-	{ "index": 1, "value": 58, "fill": "#3fae2a" },
-	{ "index": 2, "value": 14, "fill": "#95d600" },
-	{ "index": 3, "value": 5, "fill": "#c4d600" },
-	{ "index": 4, "value": 23, "fill": "#e0e67e" }
+	{ index: 0, value: 100, fill: "#00833e", label: "Total" },
+	{ index: 1, value: 58, fill: "#3fae2a", label: "10% Above" },
+	{ index: 2, value: 14, fill: "#95d600", label: "5 - 10%" },
+	{ index: 3, value: 5, fill: "#c4d600", label: "2 - 5%" },
+	{ index: 4, value: 23, fill: "#e0e67e", label: "Less than 2%" }
 ];
 
-pieChart($("#shareholders")[0], data, 75, 15, "#e3e5e9", "darkgray");
+pieChart($("#shareholders")[0], data, 75, 15, "#e3e5e9", "#a9a9a9");
