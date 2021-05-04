152592d0a84f1babdd9c0ea26bad87cedf3c7099
ZainabAl-khaboi <hime.chi@hotmail.com>
2021-05-04 14:38:48 +0400

    save

--- File Names/app.js
+++ File Names/app.js
@@ -11,6 +11,37 @@ app.use(express.static("public", { redirect: false }));
 
 var upload = multer();
 
+app.post("/test", upload.array("files"), function(req, res) {
+	var docs = req.body.docs;
+	var id = req.body.id;
+	var files = req.files;
+
+	var entries = [];
+	for (var i = 0; i < docs.length; i++) {
+		var data = JSON.parse(docs[i]);
+		data.id = crypto.createHash("md5").update(id + data.title + data.type).digest("hex");
+		data.applicantId = id;
+
+		var doc = files[i];
+		data.link = baseUrl + "job-applicants/" + id + "/" + doc.originalname;
+		data.uploadedDate = formatMySQLDate(new Date(Date.now()));
+
+		entries.push(data);
+	}
+
+	res.json(entries);
+});
+
+app.post("/test2", function(req, res) {
+	var data = req.body.data;
+	var arr = [];
+	for (var d of data) {
+		arr.push(JSON.stringify(d));
+	}
+
+	res.json({ json: arr });
+});
+
 app.listen(8000, function() {
 	console.log("File Names server running on port 8000", "\n");
 });
