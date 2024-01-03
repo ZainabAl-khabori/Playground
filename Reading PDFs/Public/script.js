59e1fda253b3700f002d565350340efb3802fd90
ZainabAl-khaboi <hime.chi@hotmail.com>
2024-01-03 13:10:38 +0400

    Deleted PDF.js

--- Reading PDFs/Public/script.js
+++ Reading PDFs/Public/script.js
@@ -1,10 +1,10 @@
-pdfjsLib.GlobalWorkerOptions.workerSrc = "/pdfjs/build/pdf.worker.js";
+// pdfjsLib.GlobalWorkerOptions.workerSrc = "/pdfjs/build/pdf.worker.js";
 
-$(document).ready(async function() {
-  var url = "http://localhost:8080/Pdfs/2018/en/balance-sheet-2018.pdf";
-  var pdf = await pdfjsLib.getDocument(url).promise;
-  var page = await pdf.getPage(1);
-  var text = await page.getTextContent();
+// $(document).ready(async function() {
+//   var url = "http://localhost:8080/Pdfs/2018/en/balance-sheet-2018.pdf";
+//   var pdf = await pdfjsLib.getDocument(url).promise;
+//   var page = await pdf.getPage(1);
+//   var text = await page.getTextContent();
 
-  console.log(text);
-});
+//   console.log(text);
+// });
--- Reading PDFs/app.js
+++ Reading PDFs/app.js
@@ -1,11 +1,31 @@
 import parser from "body-parser";
 import express from "express";
+import { readFileSync } from "fs";
+import PdfParse from "pdf-parse";
 
 var app = express();
 app.set("views", "Views");
 app.set("view engine", "ejs");
 app.use(parser.json({ limit: "50mb" }));
+
+app.use(path, express.static("node_modules/pdfjs-dist"));
 app.use(express.static("Public"));
+
+app.post("/parse", async function(req, res) {
+  var file = req.body.file;
+  var lang = req.body.lang;
+  var year = req.body.year;
+
+  var buffer = readFileSync(`Public/Pdfs/${year}/${lang}/${file}`);
+  var pdf = await PdfParse(buffer);
+  var text = pdf.text.split("\n");
+
+  res.json(text);
+});
+
+app.post("/pdfjs", async function(req, res) {
+
+});
 
 app.get("/", function(_, res) {
   var data = {
